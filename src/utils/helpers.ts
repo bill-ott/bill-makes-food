1da23a78b1f77628bb2f4d1c40163bb704ceb285
Bill Ott <willrichott@gmail.com>
2026-08-07 13:56:21 -0400

    move more functions to helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import type { CollectionEntry } from 'astro:content';
+
 function formatRawDate(date: Date) {
 	return date.toISOString().split('T')[0];
 }
@@ -11,4 +13,41 @@ function formatDate(date: Date) {
 	});
 }
 
-export { formatRawDate, formatDate };
+function sortPostsByDate<T extends { data: { publishDate: Date } }>(
+	posts: T[],
+	limit?: number,
+	direction: 'asc' | 'desc' = 'desc',
+) {
+	return [...posts]
+		.sort((a, b) => {
+			const diff = b.data.publishDate.valueOf() - a.data.publishDate.valueOf();
+			return direction === 'desc' ? diff : -diff;
+		})
+		.slice(0, limit);
+}
+
+function createEssayData(essay: CollectionEntry<'blog'>) {
+	return {
+		href: `/blog/${essay.id}`,
+		title: essay.data.title,
+		dateRaw: formatRawDate(essay.data.publishDate),
+		publishDate: formatDate(essay.data.publishDate),
+		summary: essay.data.summary,
+	};
+}
+
+function createRecipeData(recipe: CollectionEntry<'recipes'>) {
+	return {
+		href: `/recipes/${recipe.id}`,
+		title: recipe.data.title,
+		dateRaw: formatRawDate(recipe.data.publishDate),
+		publishDate: formatDate(recipe.data.publishDate),
+		cookTime: recipe.data.cookTime,
+		summary: recipe.data.summary,
+		tags: recipe.data.tags,
+		heroImage: recipe.data.heroImage,
+	};
+}
+
+
+export { sortPostsByDate, createEssayData, createRecipeData };
